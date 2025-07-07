1bed301fad549cd7125bd8f6ea77fe1909361548
cuongle512003 <cuongle512003@gmail.com>
2025-07-08 02:09:25 +0700

    Initial commit

---
+++ BE/src/main/java/com/group/prj/repository/BookingRepository.java
@@ -0,0 +1,19 @@
+package com.group.prj.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.group.prj.model.BookedRoom;
+
+@Repository
+public interface BookingRepository extends JpaRepository<BookedRoom, Long> {
+
+    Optional<BookedRoom> findByBookingConfirmationCode(String confirmationCode);
+
+    List<BookedRoom> findByRoomId(Long roomId);
+
+    List<BookedRoom> findByGuestEmail(String email);
+}
